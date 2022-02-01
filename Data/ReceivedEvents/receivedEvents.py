4346eb4c2039605a00bbbaaeb9c58b82a6caf7d0
quantumbytestudios <quantumbytestudios@gmail.com>
2022-02-01 16:14:37 +0530

    Update

--- Data/ReceivedEvents/receivedEvents.py
+++ Data/ReceivedEvents/receivedEvents.py
@@ -1,45 +1,132 @@
 # IGNORE THIS FILE !!!
 
+import json
+import requests
+
+url = "https://api.github.com/users/quantumbytestudios/received_events"
+r = requests.get(url)
+data = json.loads(r.text)
+username = "boi"
+START = 0
+END = len(data)
+f = open("index.html", "a")
+
+stylesheet = '''
+	<head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+'''
+f.write(stylesheet)
+
+for i in range(START, END):
+    # print(sep)
+    # BASIC INFO
+    ID = data[i]["id"]
+    LOGIN = data[i]["actor"]["login"]
+    AVATAR = data[i]["actor"]["avatar_url"]
+    EVENT = data[i]["type"]
+    # print("ID: "+ID)
+    # print("LOGIN: "+LOGIN)
+    # print("AVATAR: "+AVATAR)
+    # print("EVENT: "+EVENT)
+    event = data[i]["type"]
+
+    if("ForkEvent" in event):
+        # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
+        # print("USER'S REPO NAME: "+data[i]["payload"]["forkee"]["full_name"])
+        # print("USER'S REPO URL: "+data[i]["payload"]["forkee"]["html_url"])
+        forkedRepoUrl = data[i]["payload"]["forkee"]["html_url"]
+        html = f'''
+        <div class="card">
+            <div class="row">
+                <div class="col-2">
+                    <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                </div>
+                <div class="col-10">
+                    <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                    <p class="fork">Forked: <a target="_blank" href="{forkedRepoUrl}">Visit Repository</a></p>
+                </div>
+            </div>
+        </div>
+        '''
+        f.write(html)
+
+    if("WatchEvent" in event):
+        # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
+        # print("USER'S ACTION: "+data[i]["payload"]["action"])
+        repoName = data[i]["repo"]["name"]
+        html = f'''
+        <div class="card">
+            <div class="row">
+                <div class="col-2">
+                    <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                </div>
+                <div class="col-10">
+                    <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                    <p class="watch-star">Watch/Starred: <a target="_blank" href="{repoName}">Visit Repository</a></p>
+                </div>
+            </div>
+        </div>
+        '''
+        f.write(html)
+
+    if("CreateEvent" in event):
+        userRepoUrl = "https://github.com/"+data[i]["repo"]["name"]
+        # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
+        # print("USER'S REPO URL: "+userRepoUrl)
+        html = f'''
+        <div class="card">
+            <div class="row">
+                <div class="col-2">
+                    <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                </div>
+                <div class="col-10">
+                    <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                    <p class="create">Created: <a target="_blank" href="{userRepoUrl}">Visit Repository</a></p>
+                </div>
+            </div>
+        </div>
+        '''
+        f.write(html)
+
+    if("PublicEvent" in event):
+        userRepoUrl = "https://github.com/"+data[i]["repo"]["name"]
+        # print("PUBLISHED REPO URL: "+userRepoUrl)
+        html = f'''
+        <div class="card">
+            <div class="row">
+                <div class="col-2">
+                    <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                </div>
+                <div class="col-10">
+                    <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                    <p class="publish">Published: <a target="_blank" href="{userRepoUrl}">Visit Repository</a></p>
+                </div>
+            </div>
+        </div>
+        '''
+        f.write(html)
+
+    if("ReleaseEvent" in event):
+        userRepoUrl = "https://github.com/"+data[i]["payload"]["release"]["html_url"]
+        # print("RELEASED REPO URL: "+userRepoUrl)
+        html = f'''
+        <div class="card">
+            <div class="row">
+                <div class="col-2">
+                    <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                </div>
+                <div class="col-10">
+                    <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                    <p class="release">Released: <a target="_blank" href="{userRepoUrl}">Visit Repository</a></p>
+                </div>
+            </div>
+        </div>
+        '''
+        f.write(html)
 
 
 
-
-# import requests
-# import json
-# import os
-
-# os.remove("index.html")
-
-# url = "https://api.github.com/users/QuantumByteStudios/received_events"
-# r = requests.get(url)
-# r = r.text 
-
-# data = r.replace(",", "\n")
-# data2 = data.replace("{", " ")
-# data3 = data2.replace("}", " ")
-# data4 = data3.replace('"type"', '<p class="type">"TYPE"</p>')
-# data5 = data4.replace('"login"', '<p class="login">"LOGIN"</p>')
-# data6 = data5.replace('"display_login"', '<p class="login">"DISPLAY_LOGIN"</p>')
-# data7 = data6.replace('"action"', '<p class="action">"ACTION"</p>')
-# data8 = data7.replace('"html_url"', '<p class="html_url">"URL"</p>')
-# data9 = data8.replace('"repo"', '<p class="repo">"REPO"</p>')
-# data10 = data9.replace('"', ' ')
-# data11 = data10.replace('name', '<p class="name">NAME</p>')
-
-# # print(data4)
-
-# final = data11
-# f = open("index.html", "a")
-
-# stylesheet = '''
-# <head>
-#     <link rel="stylesheet" type="text/css" href="style.css">
-# </head>
-# '''
-# f.write(stylesheet)
-# f.write(final)
-
-# f.close()
-
-
-# trash = input("Ho Gaya Bhai..!")
+f.close()
+# # trash = input("Ho Gaya Bhai..!")
