3516d66ad02f24db64b629a3236e1326b166f63b
QuantumByteStudios <quantumbytestudios@gmail.com>
2023-11-22 05:30:44 +0530

    Optimized the entire source code

--- main.py
+++ main.py
@@ -1,281 +1,57 @@
-import json
-import os
-import requests
-from colorama import Fore
 import utils as session
+from utils import colors
 
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-
-
+# Clear Screen
 session.clear()
 
-
-introText = '''
+label = '''
   ____ _ _   _   _       _       _   _                 ____        _
  / ___(_) |_| | | |_   _| |__   | | | |___  ___ _ __  |  _ \  __ _| |_ __ _
 | |  _| | __| |_| | | | | '_ \  | | | / __|/ _ \ '__| | | | |/ _` | __/ _` |
 | |_| | | |_|  _  | |_| | |_) | | |_| \__ \  __/ |    | |_| | (_| | || (_| |
  \____|_|\__|_| |_|\__,_|_.__/   \___/|___/\___|_|    |____/ \__,_|\__\__,_|
 '''
-developerText = '''Tip: Don't Type "Quantum Byte Studios"\n\nCreated BY: @QuantumByteStudios\nWebsite: https://quantumbyte.studio/'''
-sepText = "\n███████████████████████████████████████████████████████\n"
+devInformation = f'''Developer: {colors.CYAN}QuantumByteStudios{colors.ENDC}\nWebsite: {colors.CYAN}https://quantumbytestudios.in{colors.ENDC}'''
 
+print(f"{colors.GREEN + label + colors.ENDC}")
+print(devInformation)
+print("\n")
 
-print(f"{bcolors.OKGREEN + introText + bcolors.ENDC}")
-print(f"{bcolors.OKCYAN + developerText + bcolors.ENDC}")
-print(f"{bcolors.FAIL + sepText + bcolors.ENDC}")
+# Get Username
+username = input(f"Enter Github Username: {colors.WARNING}")
+print(f"{colors.ENDC}")
 
-username = input("Enter Github Username: ")
-print(f"{bcolors.FAIL + sepText + bcolors.ENDC}")
-
+# Convert Username to Lowercase
 username = username.lower()
 
-if username == "quantumbytestudios":
-    print(Fore.RED + '\n\n\tBite the hand that feeds you... :( \n\n' + bcolors.ENDC)
-if username == "exit":
-    print(Fore.RED + 'Bye :P\n\n' + bcolors.ENDC)
-    exit()
-else:
-    print(
-        f"Fetching Data From API For User: {bcolors.FAIL + username + bcolors.ENDC}")
-    url = "https://api.github.com/users/"+username
-    r = requests.get(url)
-    r = r.text
-    data = r.replace("\"", " ")
-    data = r.replace("}", " ")
-    data = r.replace(",", "\n")
-    data1 = data.replace("\"", " ")
-    data2 = data1.replace("{", "")
-    data3 = data2.replace("}", "")
+urls = {
+    "mostUsedLanguages" : "\t" + "https://github-readme-stats.vercel.app/api/top-langs?username=" + username + "&langs_count=8",
+    "githubStats" : "\t" + "https://github-readme-stats.vercel.app/api?username=" + username + "&show_icons=true&locale=en",
+    "streakContributionsLS" : "\t" + "https://github-readme-streak-stats.heruapp.com/?user=" + username + "&",
+    "contributorGraphOne" : "\t" + "https://github-readme-activity-graph.vercel.app/graph?username=" + username + "&bg_color=000000&color=ffffff&line=ffffff&point=ffffff&area=true&hide_border=true",
+    "contributorGraphTwo" : "\t" + "https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=" + username+"&theme=monai", 
+}
 
-    print(f"\n{bcolors.WARNING + data3 + bcolors.ENDC}")
+def get_user_data(username):
+    if username == "exit":
+        print(colors.RED + 'Bye :P\n\n' + colors.ENDC)
+        exit()
+    else:
+        # Fetch and Print Data form API
+        session.fetchAndPrintData(username)
+        # Show Events and Graphs
+        session.showEventsAndGraphs(urls)
+        # Create HTML File User's Received Events
+        session.createAndDisplayHTMLUserEvents(username, urls)
+        # Open Generated HTML File
+        session.openResult()
+        # Exit
+        garbage = input("Press any key to exit...")
+        # Clears History
+        f = open("Data/ReceivedEvents/index.html", "a", encoding="utf_8")
+        f.truncate(0)
+        f.close()
+        print("Closing...")
+        exit()
 
-    username = username.lower()
-
-    print(f"{bcolors.OKGREEN} \n\nNow Available in Received Events {bcolors.ENDC}")
-
-    print("\n Most Used Languages: ")
-    mostUsedLanguages = "\t"+"https://github-readme-stats.vercel.app/api/top-langs?username=" + \
-        username+"&langs_count=8"
-    print(f"{bcolors.OKBLUE + mostUsedLanguages + bcolors.ENDC}")
-
-    print("\n GitHub Stats: ")
-    githubStats = "\t"+"https://github-readme-stats.vercel.app/api?username=" + \
-        username+"&show_icons=true&locale=en"
-    print(f"{bcolors.OKBLUE + githubStats + bcolors.ENDC}")
-
-    print("\n Current Streak, Total Contributions, Longest Streak: ")
-    streakContributionsLS = "\t" + \
-        "https://github-readme-streak-stats.herokuapp.com/?user="+username+"&"
-    print(f"{bcolors.OKBLUE + streakContributionsLS + bcolors.ENDC}")
-
-    print("\n Contribution Graph: ")
-    contributionGraph = "\t" + \
-        "https://github-readme-activity-graph.cyclic.app/graph?username=" + \
-        username+"&theme=github"
-    print(f"{bcolors.OKBLUE + contributionGraph + bcolors.ENDC}")
-
-    print("\n Contribution Graph (2): ")
-    secondContributionGraph = "\t" + \
-        "https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=" + \
-        username+"&theme=monokai"
-    print(f"{bcolors.OKBLUE + secondContributionGraph + bcolors.ENDC}")
-    print("\n")
-
-    print(f"{bcolors.FAIL + sepText + bcolors.ENDC}")
-
-    eventsurl = "https://api.github.com/users/"+username+"/received_events"
-    print(f"{bcolors.OKCYAN}\tEVENTS GENERATED\n\n\t\tFrom: {eventsurl}\n\t\tAT: Data/ReceivedEvents/index.html{bcolors.ENDC}")
-    ##################################################
-    os.remove("Data/ReceivedEvents/index.html")
-
-    r = requests.get(eventsurl)
-    data = json.loads(r.text)
-    START = 0
-    END = len(data)
-    f = open("Data/ReceivedEvents/index.html", "a", encoding="utf_8")
-
-    stylesheet = f'''
-	<head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    	<link rel="stylesheet" type="text/css" href="style.css">
-        <title>GUDE: {username}</title>
-        <script src="main.js"></script>
-          <!-- Fav Icons -->
-          <link rel="icon" href="https://quantumbyte.studio/Mobile/src/icon/favicon.png" type="image/x-icon">
-	</head>
-    <center>
-        <br>
-        <a href="https://github.com/QuantumByteStudios/GitHubUserDataExtracter">
-            <h2>Github User Data Extractor</h2>
-        </a>
-        <hr>
-        <h3>Receive Events of <span style="color: red;">{username}</span></h3>
-        <button id="downloadButton" class="button-30" onclick="getPDF()">Convert To PDF and Download</button>
-    </center>
-    <body>
-    <div id="cOntent">
-    '''
-    f.write(stylesheet)
-
-    for i in range(START, END):
-        # print(sep)
-        # BASIC INFO
-        ID = data[i]["id"]
-        LOGIN = data[i]["actor"]["login"]
-        AVATAR = data[i]["actor"]["avatar_url"]
-        EVENT = data[i]["type"]
-        REPO = data[i]["repo"]["name"]
-        # print("ID: "+ID)
-        # print("LOGIN: "+LOGIN)
-        # print("AVATAR: "+AVATAR)
-        # print("EVENT: "+EVENT)
-        event = data[i]["type"]
-
-        if ("ForkEvent" in event):
-            # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
-            # print("USER'S REPO NAME: "+data[i]["payload"]["forkee"]["full_name"])
-            # print("USER'S REPO URL: "+data[i]["payload"]["forkee"]["html_url"])
-            forkedRepoUrl = data[i]["payload"]["forkee"]["html_url"]
-            html = f'''
-            <div class="card">
-                <div class="row">
-                    <div class="col-2">
-                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
-                    </div>
-                    <div class="col-10">
-                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
-                        <p class="fork">Forked a repository: <a target="_blank" href="{forkedRepoUrl}">&nbsp;{REPO}</a></p>
-                    </div>
-                </div>
-            </div>
-            '''
-            f.write(html)
-
-        if ("WatchEvent" in event):
-            # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
-            # print("USER'S ACTION: "+data[i]["payload"]["action"])
-            staredRepoName = "https://github.com/"+data[i]["repo"]["name"]
-            html = f'''
-            <div class="card">
-                <div class="row">
-                    <div class="col-2">
-                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
-                    </div>
-                    <div class="col-10">
-                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
-                        <p class="watch-star">Watch/Starred a repository: <a target="_blank" href="{staredRepoName}">&nbsp;{REPO}</a></p>
-                    </div>
-                </div>
-            </div>
-            '''
-            f.write(html)
-
-        if ("CreateEvent" in event):
-            userRepoUrl = "https://github.com/"+data[i]["repo"]["name"]
-            # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
-            # print("USER'S REPO URL: "+userRepoUrl)
-            html = f'''
-            <div class="card">
-                <div class="row">
-                    <div class="col-2">
-                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
-                    </div>
-                    <div class="col-10">
-                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
-                        <p class="create">Created a repository: <a target="_blank" href="{userRepoUrl}">&nbsp;{REPO}</a></p>
-                    </div>
-                </div>
-            </div>
-            '''
-            f.write(html)
-
-        if ("PublicEvent" in event):
-            userRepoUrl = "https://github.com/"+data[i]["repo"]["name"]
-            # print("PUBLISHED REPO URL: "+userRepoUrl)
-            html = f'''
-            <div class="card">
-                <div class="row">
-                    <div class="col-2">
-                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
-                    </div>
-                    <div class="col-10">
-                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
-                        <p class="publish">Published a repository: <a target="_blank" href="{userRepoUrl}">&nbsp;{REPO}</a></p>
-                    </div>
-                </div>
-            </div>
-            '''
-            f.write(html)
-
-        if ("ReleaseEvent" in event):
-            userRepoUrl = data[i]["payload"]["release"]["html_url"]
-            # print("RELEASED REPO URL: "+userRepoUrl)
-            html = f'''
-            <div class="card">
-                <div class="row">
-                    <div class="col-2">
-                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
-                    </div>
-                    <div class="col-10">
-                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
-                        <p class="release">Released a repository: <a target="_blank" href="{userRepoUrl}">&nbsp;{REPO}</a></p>
-                    </div>
-                </div>
-            </div>
-            '''
-            f.write(html)
-
-    userStats = f'''
-    <br>
-    <div class="container stats">
-        <center>
-            <div class="row">
-                    <br>
-                    <div class="col-md-12">
-                        <img src="{mostUsedLanguages}" alt="GitHubUserDataExtractor">
-                    </div>
-                    <br>
-                    <div class="col-md-12">
-                        <img src="{githubStats}" alt="GitHubUserDataExtractor">
-                    </div>
-                    <br>
-                    <div class="col-md-12">
-                        <img src="{streakContributionsLS}" alt="GitHubUserDataExtractor"><br>
-                        <br><br>
-                    </div>                    
-                    <hr><br><br>
-                    <div class="col-md-12">
-                        <img class="imgCustStyle" src="{contributionGraph}" alt="GitHubUserDataExtractor"><br>
-                    </div>
-                    <br>
-                    <div class="col-md-12">
-                        <img class="imgCustStyle" src="{secondContributionGraph}" alt="GitHubUserDataExtractor"><br>
-                    </div>
-                    <br>
-            </div>
-            <br>
-        </center>
-    </div>
-    '''
-    f.write(userStats)
-    f.close()
-
-    session.openResult()
-
-    garbage = input("Press any key to exit...")
-    # Clears History
-    f = open("Data/ReceivedEvents/index.html", "a", encoding="utf_8")
-    f.truncate(0)
-    f.close()
-    print("Closing...")
-    exit()
+get_user_data(username)
--- utils.py
+++ utils.py
@@ -1,18 +1,239 @@
-import subprocess
+import json
+import os
 import platform
+import requests
+import subprocess
+from colorama import Fore
 
+# Colors
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
 
+# Clear Screen
 def clear():
     if platform.system() == "Windows":
         subprocess.run("cls", shell=True)  # nosec B602, B607
     else:
         subprocess.run("clear", shell=True)  # nosec B602, B607
 
-
+# Open Result
 def openResult():
     if platform.system() == "Windows":
-        subprocess.run('start Data/ReceivedEvents/boot.html',
-                       shell=True)  # nosec B602, B607
+        subprocess.run('start Data/ReceivedEvents/boot.html', shell=True)  # nosec B602, B607
     else:
-        subprocess.run(
-            'open Data/ReceivedEvents/boot.html 2>/dev/null', shell=True)  # nosec B602, B607
+        subprocess.run('open Data/ReceivedEvents/boot.html 2>/dev/null', shell=True)  # nosec B602, B607
+
+
+# Fetch and Print Data form API
+def fetchAndPrintData(username):
+    print(f"Fetching Data From API For User: {colors.FAIL + username + colors.ENDC}")
+    url = "https://api.github.com/users/"+username
+    r = requests.get(url).text
+    data = r.replace("\"", " ").replace("}", " ").replace(",", "\n").replace("\"", " ").replace("{", "").replace("}", "")
+    print(f"\n{colors.WARNING + data + colors.ENDC}")
+
+# Show Events and Graphs
+def showEventsAndGraphs(urls):
+    # Start
+    print(f"\n\nGraphs are now available in Received Events [ {colors.GREEN}✓{colors.ENDC} ]")
+
+    # Most Used Languages
+    # print("\n Most Used Languages: ")
+    # print(f"{colors.BLUE + urls['mostUsedLanguages'] + colors.ENDC}")
+
+    # GitHub Stats
+    # print("\n GitHub Stats: ")
+    # print(f"{colors.BLUE + urls['githubStats'] + colors.ENDC}")
+
+    # Current Streak, Total Contributions, Longest Streak
+    # print("\n Current Streak, Total Contributions, Longest Streak: ")
+    # print(f"{colors.BLUE + urls['streakContributionsLS'] + colors.ENDC}")
+
+    # Contribution Graphs One & Two
+    # print("\n Contribution Graph: (One)")
+    # print(f"{colors.BLUE + urls['contributorGraphOne'] + colors.ENDC}")
+    # print("\n Contribution Graph: (Two)")
+    # print(f"{colors.BLUE + urls['contributorGraphTwo'] + colors.ENDC}")
+    
+    # End
+    # print("\n\n")
+
+# HTML Received Events File
+def createAndDisplayHTMLUserEvents(username, urls):
+    eventsurl = "https://api.github.com/users/"+username+"/received_events"
+    # print(f"{colors.CYAN}\tEVENTS GENERATED\n\n\t\tFrom: {eventsurl}\n\t\tAT: Data/ReceivedEvents/index.html{colors.ENDC}")
+    print(f"HTML received events generated successfully [ {colors.GREEN}✓{colors.ENDC} ]\n\n")
+        
+    # Delete Old File
+    if os.path.exists("Data/ReceivedEvents/index.html"):
+        os.remove("Data/ReceivedEvents/index.html")
+    r = requests.get(eventsurl).text
+    data = json.loads(r)
+    START = 0
+    END = len(data)
+    f = open("Data/ReceivedEvents/index.html", "a", encoding="utf_8")
+    stylesheet = f'''
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>GUDE: {username}</title>
+        <script src="main.js"></script>
+        <!-- Fav Icons -->
+        <link rel="icon" href="https://quantumbyte.studio/Mobile/src/icon/favicon.png" type="image/x-icon">
+    </head>
+    <center>
+        <br>
+        <a href="https://github.com/QuantumByteStudios/GitHubUserDataExtracter">
+            <h2>Github User Data Extractor</h2>
+        </a>
+        <hr>
+        <h3>Receive Events of <span style="color: red;">{username}</span></h3>
+        <button id="downloadButton" class="button-30" onclick="getPDF()">Convert To PDF and Download</button>
+    </center>
+    <body>
+    <div id="cOntent">
+    '''
+    f.write(stylesheet)
+    for i in range(START, END):
+        # print(sep)
+        # BASIC INFO
+        ID = data[i]["id"]
+        LOGIN = data[i]["actor"]["login"]
+        AVATAR = data[i]["actor"]["avatar_url"]
+        EVENT = data[i]["type"]
+        REPO = data[i]["repo"]["name"]
+        # print("ID: "+ID)
+        # print("LOGIN: "+LOGIN)
+        # print("AVATAR: "+AVATAR)
+        # print("EVENT: "+EVENT)
+        event = data[i]["type"]
+        if ("ForkEvent" in event):
+            # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
+            # print("USER'S REPO NAME: "+data[i]["payload"]["forkee"]["full_name"])
+            # print("USER'S REPO URL: "+data[i]["payload"]["forkee"]["html_url"])
+            forkedRepoUrl = data[i]["payload"]["forkee"]["html_url"]
+            html = f'''
+            <div class="card">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                    </div>
+                    <div class="col-10">
+                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                        <p class="fork">Forked a repository: <a target="_blank" href="{forkedRepoUrl}">&nbsp;{REPO}</a></p>
+                    </div>
+                </div>
+            </div>
+            '''
+            f.write(html)
+        if ("WatchEvent" in event):
+            # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
+            # print("USER'S ACTION: "+data[i]["payload"]["action"])
+            staredRepoName = "https://github.com/"+data[i]["repo"]["name"]
+            html = f'''
+            <div class="card">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                    </div>
+                    <div class="col-10">
+                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                        <p class="watch-star">Watch/Starred a repository: <a target="_blank" href="{staredRepoName}">&nbsp{REPO}</a></p>
+                    </div>
+                </div>
+            </div>
+            '''
+            f.write(html)
+        if ("CreateEvent" in event):
+            userRepoUrl = "https://github.com/"+data[i]["repo"]["name"]
+            # print("ORIGINAL REPO NAME: "+data[i]["repo"]["name"])
+            # print("USER'S REPO URL: "+userRepoUrl)
+            html = f'''
+            <div class="card">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                    </div>
+                    <div class="col-10">
+                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                        <p class="create">Created a repository: <a target="_blank" href="{userRepoUrl}">&nbsp;{REPO}</a></p>
+                    </div>
+                </div>
+            </div>
+            '''
+            f.write(html)
+        if ("PublicEvent" in event):
+            userRepoUrl = "https://github.com/"+data[i]["repo"]["name"]
+            # print("PUBLISHED REPO URL: "+userRepoUrl)
+            html = f'''
+            <div class="card">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                    </div>
+                    <div class="col-10">
+                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                        <p class="publish">Published a repository: <a target="_blank" href="{userRepoUrl}">&nbsp;{REPO}</a><p>
+                    </div>
+                </div>
+            </div>
+            '''
+            f.write(html)
+        if ("ReleaseEvent" in event):
+            userRepoUrl = data[i]["payload"]["release"]["html_url"]
+            # print("RELEASED REPO URL: "+userRepoUrl)
+            html = f'''
+            <div class="card">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="{AVATAR}" class="img-fluid profilePicture" alt="ProfilePicture">
+                    </div>
+                    <div class="col-10">
+                        <a href="https://github.com/{LOGIN}"><p>{LOGIN}</p></a>
+                        <p class="release">Released a repository: <a target="_blank" href="{userRepoUrl}">&nbsp;{REPO}</a></p>
+                    </div>
+                </div>
+            </div>
+            '''
+            f.write(html)
+    userStats = f'''
+    <br>
+    <div class="container stats">
+        <center>
+            <div class="row">
+                <br>
+                <div class="col-md-12">
+                    <img src="{urls["mostUsedLanguages"]}" alt="GitHubUserDataExtractor">
+                </div>
+                <br>
+                <div class="col-md-12">
+                    <img src="{urls["githubStats"]}" alt="GitHubUserDataExtractor">
+                </div>
+                <br>
+                <div class="col-md-12">
+                    <img src="{urls["streakContributionsLS"]}" alt="GitHubUserDataExtractor"><br>
+                    <br><br>
+                </div>                    
+                <hr><br><br>
+                <div class="col-md-12">
+                    <img class="imgCustStyle" src="{urls["contributorGraphOne"]}" alt="GitHubUserDataExtractor"><br>
+                </div>
+                <br>
+                <div class="col-md-12">
+                    <img class="imgCustStyle" src="{urls["contributorGraphTwo"]}" alt="GitHubUserDataExtractor"><br>
+                </div>
+                <br>
+            </div>
+            <br>
+        </center>
+    </div>
+    '''
+    f.write(userStats)
+    f.close()
